9fc179c8c1f072e52c658ad7e1facbdda95ece9a
Hammad774 <hammad24197@gmail.com>
2025-06-01 01:48:19 +0500

    update

--- src/Layout/appLayout.jsx
+++ src/Layout/appLayout.jsx
@@ -8,10 +8,10 @@ const AppLayout = () => {
        backgroundImage: 'linear-gradient(to bottom right, #d1fae5, #bbf7d0)'}}
       >
       {/* Header */}
-      <header className=" text-green-800 font-[cursive] flex items-center justify-between ">
+      <header className=" text-green-800 font-[cursive] flex items-center justify-between">
         <div className="flex items-center gap-2">
           {/* <img src={logo} alt="Logo" className="h-10 w-10 object-contain" /> */}
-          <h1 className="text-sm md:text-xl font-bold"><a href='https://www.hammadmeer.netlify.app'>Eid ul Adha Wishes</a></h1>
+          <h1 className="text-sm md:text-xl font-bold"><a href='https://hammadmeer.netlify.app/'>Eid ul Adha Wishes</a></h1>
         </div>
         <nav className="flex gap-4">
           <Link to="/" className="hover:text-green-950 text-green-800">Home</Link>
